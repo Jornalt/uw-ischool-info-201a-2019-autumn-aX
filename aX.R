e9f5f83515ff84042e5d05d08630e495778bef8a
Joseph <joseph.altamira@gmail.com>
2019-12-06 21:36:13 -0800

    slight changes for clarity

--- aX.R
+++ aX.R
@@ -6,27 +6,27 @@ library(lubridate)
 
 df_epa <- read.csv("epa_date.csv", stringsAsFactors = FALSE, na.rm = TRUE)
 
-ggplot(df_epa, aes(x = as.Date(Date), y = CO, color = "CO")) + 
+CO_plot <- ggplot(df_epa, aes(x = as.Date(Date), y = CO, color = "CO")) + 
   geom_line() +
   geom_smooth(method = "lm", alpha = .15)
 
-ggplot(df_epa, aes(x = as.Date(Date), y = LC25, color = "LC25")) + 
+LC25_plot <- ggplot(df_epa, aes(x = as.Date(Date), y = LC25, color = "LC25")) + 
   geom_line() +
   geom_smooth(method = "lm", alpha = .15)
 
-ggplot(df_epa, aes(x = as.Date(Date), y = NO2, color = "NO2")) + 
+NO2_plot <- ggplot(df_epa, aes(x = as.Date(Date), y = NO2, color = "NO2")) + 
   geom_line() +
   geom_smooth(method = "lm", alpha = .15)
 
-ggplot(df_epa, aes(x = as.Date(Date), y = O3, color = "O3")) + 
+O3_plot <- ggplot(df_epa, aes(x = as.Date(Date), y = O3, color = "O3")) + 
   geom_line() +
   geom_smooth(method = "lm", alpha = .15)
 
-ggplot(df_epa, aes(x = as.Date(Date), y = PM10, color = "PM10")) + 
+PM10_plot <- ggplot(df_epa, aes(x = as.Date(Date), y = PM10, color = "PM10")) + 
   geom_line() +
   geom_smooth(method = "lm", alpha = .15)
 
-ggplot(df_epa, aes(x = as.Date(Date), y = SO2, color = "SO2")) + 
+SO2_plot <- ggplot(df_epa, aes(x = as.Date(Date), y = SO2, color = "SO2")) + 
   geom_line() +
   geom_smooth(method = "lm", alpha = .15)
         
